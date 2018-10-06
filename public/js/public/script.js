02a8336a3171e3c53ed6fc560085c87de2671a27
at-namtran <nam.tran@asiantech.vn>
2018-10-06 21:48:44 +0700

    api profile

--- public/js/public/script.js
+++ public/js/public/script.js
@@ -5,41 +5,22 @@ $(document).ready(function(){
 	
   showProfile();
   
-  if (localStorage.getItem('user_booking')) {
-    showBooking();
-  }
-	/* This code is executed after the DOM has been completely loaded */
-
-	/* Changing thedefault easing effect - will affect the slideUp/slideDown methods: */
-	$.easing.def = "easeOutBounce";
-
-	/* Binding a click event handler to the links: */
-	$('li.button a').click(function(e){
-	
-		/* Finding the drop down list that corresponds to the current section: */
-		var dropDown = $(this).parent().next();
-		
-		/* Closing all other drop down sections, except the current one */
-		$('.dropdown').not(dropDown).slideUp('slow');
-		dropDown.slideToggle('slow');
-		
-		/* Preventing the default event (which would be to navigate the browser to the link's address) */
-		e.preventDefault();
-	})
-	
+  // if (localStorage.getItem('user_booking')) {
+  //   showBooking();
+  // }
 });
 
 function showProfile() {
 	var userProfile = JSON.parse(localStorage.getItem('user'));
-	var role;
-	if (userProfile.role === 1) {
-		role = "Admin";
-	} else {
-		role = "User";
-	}
-	$('.teddy-text h4').text(userProfile.full_name);
-	$('.teddy-text p').text(role);
-	$('#name').text(userProfile.full_name);
+	// var role;
+	// if (userProfile.role === 1) {
+	// 	role = "Admin";
+	// } else {
+	// 	role = "User";
+	// }
+	// $('.teddy-text h4').text(userProfile.full_name);
+	// $('.teddy-text p').text(role);
+	$('#full_name').text(userProfile.full_name);
 	$('#email').text(userProfile.email);
 	$('#phone').text(userProfile.phone);
 	$('#address').text(userProfile.address);
